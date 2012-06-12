338d8f45724770a4be3aa02f30cda2f1592faed6
Alan Kligman <alan.kligman@gmail.com>
2012-06-12 12:38:13 -0400

    Added stub files for dispatcher tests.

---
+++ src/services/dispatcher.test.js
@@ -0,0 +1,14 @@
+define(
+  [ "src/services/dispatcher" ],
+  function( Dispatcher ) {
+    return function() {
+
+      module( "Dispatcher", {
+        setup: function() {
+        },
+        teardown: function() {}
+      });
+      
+    };
+  }
+);
